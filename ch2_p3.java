e569d0452ad87e546ce3fe7cc68c9e07e1e68fcf
Ramya K P <ramyakprammu@gmail.com>
2024-12-28 17:39:15 +0530

    i learnt variables and datatypes

---
+++ ch2_p3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ch2_p3 {
+    public static void main(String[] args){
+        int m=990;
+        int m1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int n=sc.nextInt();
+        char ch = sc.next().charAt(0);
+        System.out.println(n>m);
+        System.out.println(ch);
+       
+
+
+
+    }
+}
